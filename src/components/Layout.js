cc9f2028583d878a3541c2db4fe2d9b4938dbc01
EDILSONJOSE26 <edilsonjoseex@gmail.com>
2023-04-23 13:32:35 -0300

    atualizando

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Fooder  from "./Footer";
+import Footer  from "./Footer";
 import Header  from "./Header";
 const Layout = () => {
     return (
